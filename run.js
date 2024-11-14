f3911160bda6e4c036cfca1eff34868326ace5bc
Maksim <mowgzem@gmail.com>
2024-11-14 15:58:33 +0200

    Initial commit

--- run.js
+++ run.js
@@ -6,12 +6,68 @@ import inquirer from 'inquirer';
 
 inquirer
     .prompt([
+        
         {
-            type:'list',
+            type: 'input',
+            message: 'Who are you?',
+            name: 'name',
+        },
+        
+        {
+            type: 'number',
+            message: 'How old are you?',
+            name: 'age',
+        },
+        
+        {
+            type: 'confirm',
+            message: 'Are you a human?',
+            name: 'confirmhuman',
+        },
+
+        {
+            type: 'list',
             message: 'Favorite fruit?',
-            name:'fruit',
+            name: 'fruit',
             choices: ['Apple', 'Cherry', 'Mango', 'Pear'],
-        }
+        },
+
+        {
+            type: 'rawlist',
+            message: 'Choose a movie',
+            name: 'choosemovie',
+            choices: ['Spider-man 2', 'The Silence of the Lambs', 'Trainspotting', 'The Matrix'],
+        },
+        
+        {
+            type: 'expand',
+            message: 'Do you want to continue?',
+            name: 'continue',
+            choices: [
+                { key: 'y', name: 'yes', value: 'yes' },
+                { key: 'n', name: 'no', value: 'no' },
+            ],
+        },
+        
+        {
+            type: 'checkbox',
+            message: 'Whose games do you prefer',
+            name: 'games',
+            choices: ['Valve', 'Ubisoft', 'EA', 'Blizzard', 'Sony'],
+        },
+        
+        {
+            type: 'password',
+            message: 'Enter your password:',
+            name: 'password',
+            mask: '*', 
+        },
+        
+        {
+            type: 'editor',
+            message: 'Write about yourself:',
+            name: 'aboutyou',
+        },
     ])
     .then((answers) => {
         console.log(answers);
@@ -20,6 +76,6 @@ inquirer
         if (error.isTtyError) {
             // Prompt couldn't be rendered in the current environment
         } else {
-            // Something else went wrong
+               // Something else went wrong
         }
     });
